21eb988dd6bdb295b03a4cc0b016eff78f66aaf0
Jose Landa <jlanda4@wgu.edu>
2023-11-15 17:06:24 -0600

    Pulled Excel cell data to populate PDF file cells, populated sum of all costs, added name and logo

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pathlib import Path
 filepaths = glob.glob("invoices/*.xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
 
@@ -17,6 +17,51 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f"Invoice Number: {invoice_num}", ln=1)
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice Date: {invoice_date}")
+    pdf.cell(w=50, h=8, txt=f"Invoice Date: {invoice_date}", ln=1)
+    pdf.cell(w=30, h=8, txt=" ", ln=1)
 
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # add header
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=50, h=8, txt=columns[1], border=1)
+    pdf.cell(w=40, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    # add rows
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=50, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=40, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=50, h=8, txt="", border=1)
+    pdf.cell(w=40, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Add total amount due text
+    pdf.set_font(family="Times", size=20, style="B")
+    pdf.cell(w=30, h=8, txt=" ", ln=1)
+    pdf.cell(w=30, h=8, txt=f"The total amount due is {total_sum}", ln=2)
+    pdf.cell(w=30, h=8, txt=" ", ln=1)
+
+    # Add name and logo
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=30, h=8, txt=f"Jose Landa")
+    pdf.image("pdf_image.png", w=10)
+
+    # Generate the PDF
     pdf.output(f"PDFs/{filename}.pdf")
